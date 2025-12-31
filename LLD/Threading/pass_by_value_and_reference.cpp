650c83bc725a30508d29cec9f6c8d47ac3a67922
Ashish Balwada <57625151+AshishBalwada22@users.noreply.github.com>
2025-12-31 11:26:39 +0000

    lambda value and reference

--- LLD/Threading/pass_by_value_and_reference.cpp
+++ LLD/Threading/pass_by_value_and_reference.cpp
@@ -8,7 +8,10 @@ void counter(int &x){
 int main(){
     int cnt=0;
     // thread t(counter,cnt); // pass by value
-    thread t(counter,ref(cnt)); //pass by reference
+    //thread t(counter,ref(cnt)); //pass by reference
+    thread t([&](){ // & for reference , = for by value 
+         cnt++;
+    });
     t.join();
     cout<<"cnt = "<<cnt<<endl;
     cout<<"ends here"<<endl;
