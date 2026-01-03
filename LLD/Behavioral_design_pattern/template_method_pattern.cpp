493f0adb8a7c71cbb7a12528fa416cce78c329d3
Ashish Balwada <57625151+AshishBalwada22@users.noreply.github.com>
2026-01-03 13:01:11 +0000

    template method

---
+++ LLD/Behavioral_design_pattern/template_method_pattern.cpp
@@ -0,0 +1,3 @@
+// template method pattern
+// game -> play() , all other functions are implemented by inherited class, fixed number of steps.
+// defines the skeleton of an algorithm in an operation, deferring some steps to subclasses
